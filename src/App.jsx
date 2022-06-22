2fae62b2154817807212a1ba021d345228eaf698
Rafael Braga <rafaelbra149@gmail.com>
2022-06-22 20:18:20 +0100

    Interaction

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 import './global.css';
 
+const post = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diebraga.png',
+      name: 'Diego Braga',
+      role: 'Full Stack Developer @Hertz'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('22-06-2022 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Connor Murphy',
+      role: 'Educator @Hertz'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('24-06-2022 20:00:00')
+  }
+];
+
 export function App() {
   return (
     <div>
@@ -13,14 +44,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author = 'Rafael Braga' 
-            content= 'Life is beautiful. I am the bex that contains the box'
-          />
-          <Post 
-            author = 'Rafael Murphy' 
-            content= 'The same as I am the dreamer and the persond doing the dream'
-          />
+          {post.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,20 +1,24 @@
+import { format } from 'date-fns';
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post () {
+export function Post ({ author, publishedAt }) {
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src='https://github.com/diebraga.png' />
+                    <Avatar src={author.avatarUrl} /> 
                     <div className={styles.authorInfo}>
-                         <strong>Diego Braga</strong>
-                         <span>Full Stack Developer</span>
+                         <strong>{author.name}</strong>
+                         <span>{author.role}</span>
                     </div>
                 </div>
                 
-                <time title='20 June at 08:13h' dateTime= '2022-06-20 08:13:30'>Published 1h ago</time>
+                <time title='20 June at 08:13h'>
+                    Published 1h ago
+                </time>
             </header>
             <div className={styles.content}>
                 <p>Fala galeraa 👋</p>
