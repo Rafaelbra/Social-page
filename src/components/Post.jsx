1cf98a38be7cbcebe93b6e43c2bb7e2291faa0de
Rafael Braga <rafaelbra149@gmail.com>
2022-06-23 23:24:40 +0100

    Removing-comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -23,6 +23,14 @@ export function Post ({ author, content }) {
         setNewCommentText(event.target.value);
     }
 
+    function deleteComment (commentsToDelete ) {
+        const commentsWithoutDeletedOnes = comments.filter(comment => {
+            return comment !== commentsToDelete
+        })
+        
+        setComments(commentsWithoutDeletedOnes);
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -41,9 +49,9 @@ export function Post ({ author, content }) {
             <div className={styles.content}>
                 {content.map(line => {
                     if (line.type == 'paragraph') {
-                        return <p>{line.content}</p>;
+                        return <p key={line.content}>{line.content}</p>;
                     } else if (line.type == 'link') {
-                        return <p><a href='#'>{line.content}</a></p>
+                        return <p key={line.content}><a href='#'>{line.content}</a></p>
                     }
                 })} 
             </div>
@@ -65,7 +73,13 @@ export function Post ({ author, content }) {
 
             <div className={styles.commentList}>
               {comments.map(comment => {
-                  return <Comment content={comment} />
+                  return (
+                    <Comment 
+                        key={comment} 
+                        content={comment} 
+                        onDeleteComment={deleteComment} 
+                    />
+                )
               })}
             </div>
         </article>
